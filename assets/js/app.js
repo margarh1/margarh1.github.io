0df53d7bf83d6382e1e9b0bd1b7702bdaafd02d5
margarh1 <ha.margaret1@gmail.com>
2016-11-07 14:32:45 -0800

    fiddled around with adding handlebars and ajax to website

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,26 @@
-// sanity check
 console.log("app.js linked.");
+
+$(document).ready(function() {
+  $.ajax({
+    method: 'GET',
+    url: 'https://api.github.com/users/margarh1/repos',
+    success: onSuccess,
+    error: onError,
+    complete: console.log('Complete')
+  })
+
+  function onSuccess(json) {
+    var projectSource = $('#project-template').html();
+    var projectTemplate = Handlebars.compile(projectSource);
+    var projectHtml = projectTemplate({ repos: json });
+    console.log(projectHtml);
+    $('.works').append(projectHtml);
+  }
+
+  function onError(xhr, status, errorThrown) {
+    alert('Sorry, there was a problem!');
+    console.log('Error: ' + errorThrown);
+    console.log('Status: ' + status);
+    console.dir(xhr);
+  }
+})
