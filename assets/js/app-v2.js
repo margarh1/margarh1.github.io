9705f721babbc725971d1dcc50e5c3ad667cc6e9
margarh1 <ha.margaret1@gmail.com>
2016-11-28 17:47:01 -0800

    made some changes to index.html and main.css that I don't recall anymore, also wrote new versions of index.html, main.css, and app.js that I'm much happier with

---
+++ assets/js/app-v2.js
@@ -0,0 +1,25 @@
+console.log("app.js linked.");
+
+$(document).ready(function() {
+  $.ajax({
+    method: 'GET',
+    url: 'https://api.github.com/users/margarh1/repos',
+    success: onSuccess,
+    error: onError,
+    complete: console.log('Complete')
+  });
+});
+
+  function onSuccess(json) {
+    var projectSource = $('#project-template').html();
+    var projectTemplate = Handlebars.compile(projectSource);
+    var projectHtml = projectTemplate({ repos: json });
+    $('.row').append(projectHtml);
+  };
+
+  function onError(xhr, status, errorThrown) {
+    alert('Sorry, there was a problem!');
+    console.log('Error: ' + errorThrown);
+    console.log('Status: ' + status);
+    console.dir(xhr);
+  };
